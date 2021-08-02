81c5097494cda34e193b4d33dd558853c057a59b
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-08-02 10:40:01 -0300

    Utilizando StyleSheet

--- 01_myskills/src/pages/Home.js
+++ 01_myskills/src/pages/Home.js
@@ -1,10 +1,56 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Platform,
+  TouchableOpacity
+} from "react-native";
 
 export function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#21222c' }}>
-      <Text style={{ fontSize: 24, color: '#fff' }}>Daniel Vinícius</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Welcome, Daniel</Text>
+      <TextInput style={styles.input} placeholder="New Skill" placeholderTextColor="#555" />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+      <Text style={[styles.title, { marginTop: 50 }]}>My Skills</Text>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingVertical: 70,
+    paddingHorizontal: 30
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold'
+  }
+});
