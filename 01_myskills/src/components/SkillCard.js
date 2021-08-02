e037cef763da755a9ca22b2297772b39ac2111eb
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-08-02 12:12:58 -0300

    Scroll View x FlatList

---
+++ 01_myskills/src/components/SkillCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet
+} from "react-native";
+
+export function SkillCard({ skill }) {
+  return (
+    <TouchableOpacity style={styles.buttonSkill} activeOpacity={0.7}>
+      <Text style={styles.textSkill}>{skill}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonSkill: {
+    backgroundColor: '#1f1e25',
+    padding: 15,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginVertical: 10
+  },
+  textSkill: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold'
+  }
+});
--- 01_myskills/src/pages/Home.js
+++ 01_myskills/src/pages/Home.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Text,
   StyleSheet,
-  SafeAreaView,
+  View,
   TextInput,
   Platform,
-  TouchableOpacity
+  FlatList
 } from "react-native";
 
+import { Button } from "../components/Button";
+import { SkillCard } from "../components/SkillCard";
+
 export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill() {
+    if (newSkill.trim()) {
+      setMySkills(oldState => [...oldState, newSkill]);
+      setNewSkill('');
+    }
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.title}>Welcome, Daniel</Text>
-      <TextInput style={styles.input} placeholder="New Skill" placeholderTextColor="#555" />
-      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-        <Text style={styles.buttonText}>Add</Text>
-      </TouchableOpacity>
-      <Text style={[styles.title, { marginTop: 50 }]}>My Skills</Text>
-    </SafeAreaView>
+      <TextInput
+        style={styles.input}
+        placeholder="New Skill"
+        placeholderTextColor="#555"
+        value={newSkill}
+        onChangeText={setNewSkill}
+      />
+
+      <Button onPress={handleAddNewSkill} />
+      <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
+
+      <FlatList 
+        data={mySkills}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <SkillCard skill={item} />}/>
+    </View>
   );
 };
 
@@ -40,17 +64,5 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 30,
     borderRadius: 7
-  },
-  button: {
-    backgroundColor: '#a370f7',
-    padding: 15,
-    borderRadius: 7,
-    alignItems: 'center',
-    marginTop: 20
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 17,
-    fontWeight: 'bold'
   }
 });
