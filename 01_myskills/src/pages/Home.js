c7dc53492ecabe66daac0db081432dc56d6dd67c
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-08-02 10:16:49 -0300

    Estrutura JSX

---
+++ 01_myskills/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#21222c' }}>
+      <Text style={{ fontSize: 24, color: '#fff' }}>Daniel Vinícius</Text>
+    </View>
+  );
+};
