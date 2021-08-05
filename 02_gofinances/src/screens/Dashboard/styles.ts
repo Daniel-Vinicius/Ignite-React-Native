b46935231612d82fff657cf2bcfac943438c93e2
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-08-05 20:07:39 -0300

    Criando estilos globais

--- 02_gofinances/src/screens/Dashboard/styles.ts
+++ 02_gofinances/src/screens/Dashboard/styles.ts
@@ -4,11 +4,11 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  background: #007BC6;
+  background: ${(props) => props.theme.colors.background};
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   font-weight: bold;
-  color: #fff;
+  color: ${(props) => props.theme.colors.title};
 `;
