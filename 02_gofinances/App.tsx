520b91ee3a1f5a30e3352fa2f781d00ca68b917e
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-08-05 19:46:38 -0300

    Conhecendo o Styled Components

--- 02_gofinances/App.tsx
+++ 02_gofinances/App.tsx
@@ -1,22 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Welcome } from './src/components/Welcome';
+
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Welcome title="Welcome" />
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Dashboard />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#89b9b9',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ 02_gofinances/src/screens/Dashboard/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background: #007BC6;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+`;
