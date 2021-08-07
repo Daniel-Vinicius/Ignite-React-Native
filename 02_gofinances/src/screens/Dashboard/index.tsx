3ba872035cd0a55eb988fb31f50a4dff7fc6856b
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-08-07 10:04:20 -0300

    Utilizando proporções em medidas

--- 02_gofinances/src/screens/Dashboard/index.tsx
+++ 02_gofinances/src/screens/Dashboard/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import { Container, Title } from './styles';
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/66279500?v=4' }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Daniel</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
