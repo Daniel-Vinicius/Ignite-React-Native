e987bd1ef67ac51e1d27e3cb72bfad7c1e2dabc1
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-08-15 09:41:15 -0300

    Estilização do filtro por mês

--- 02_gofinances/src/screens/Resume/index.tsx
+++ 02_gofinances/src/screens/Resume/index.tsx
@@ -3,6 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { VictoryPie } from 'victory-native';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { useTheme } from 'styled-components';
 
 import { HistoryCard } from '../../components/HistoryCard';
@@ -19,6 +20,10 @@ import {
   Title,
   Content,
   ChartContainer,
+  MonthSelect,
+  MonthSelectButton,
+  Month,
+  MonthSelectIcon,
 } from './styles';
 
 interface CategoryData {
@@ -82,7 +87,22 @@ export function Resume() {
         <Title>Resumo por categoria</Title>
       </Header>
 
-      <Content>
+      <Content
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 24, paddingBottom: useBottomTabBarHeight() }}
+      >
+        <MonthSelect>
+          <MonthSelectButton>
+            <MonthSelectIcon name="chevron-left" />
+          </MonthSelectButton>
+
+          <Month>Maio</Month>
+
+          <MonthSelectButton>
+            <MonthSelectIcon name="chevron-right" />
+          </MonthSelectButton>
+        </MonthSelect>
+
         <ChartContainer>
           <VictoryPie
             data={totalByCategories}
--- 02_gofinances/src/screens/Resume/styles.ts
+++ 02_gofinances/src/screens/Resume/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components/native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 
 import { RFValue } from 'react-native-responsive-fontsize';
 
@@ -25,12 +27,31 @@ export const Title = styled.Text`
   color: ${({ theme }) => theme.colors.shape};
 `;
 
-export const Content = styled.ScrollView.attrs({
-  contentContainerStyle: { flex: 1, padding: 24 }
-})``;
+export const Content = styled.ScrollView``;
 
 export const ChartContainer = styled.View`
   width: 100%;
   align-items: center;
+`;
+
+export const MonthSelect = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-top: 24px;
+`;
+
+export const MonthSelectButton = styled(BorderlessButton)``;
+
+export const Month = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${RFValue(20)}px;
+`;
+
+export const MonthSelectIcon = styled(Feather)`
+  font-size: ${RFValue(24)}px;
 `;
 
