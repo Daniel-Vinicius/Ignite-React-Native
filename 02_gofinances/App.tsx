261fd380c7461d5447017a94aacd0f29530cb9f9
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-08-16 10:00:05 -0300

    Interface de Autenticação

--- 02_gofinances/App.tsx
+++ 02_gofinances/App.tsx
@@ -13,7 +13,8 @@ import {
 
 import theme from './src/global/styles/theme';
 
-import { AppRoutes } from './src/routes/app.routes';
+// import { AppRoutes } from './src/routes/app.routes';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -29,7 +30,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
       <StatusBar style="light" translucent />
     </ThemeProvider>
