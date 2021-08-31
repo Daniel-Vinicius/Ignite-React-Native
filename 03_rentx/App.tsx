b8e9d63b2aa680024ebc265b3bd1e4c57e57b114
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-08-30 23:56:36 -0300

    Interface de Agendamento Concluído

--- 03_rentx/App.tsx
+++ 03_rentx/App.tsx
@@ -11,6 +11,7 @@ import { Home } from './src/screens/Home';
 import { CarDetails } from './src/screens/CarDetails';
 import { Scheduling } from './src/screens/Scheduling';
 import { SchedulingDetails } from './src/screens/SchedulingDetails';
+import { SchedulingComplete } from './src/screens/SchedulingComplete';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +28,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   );
 }
