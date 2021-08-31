228ab44e701a1612fcafa42ac7d6837852168e16
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-08-30 22:12:00 -0300

    Componente de Calendário

--- 03_rentx/src/screens/Scheduling/index.tsx
+++ 03_rentx/src/screens/Scheduling/index.tsx
@@ -4,6 +4,7 @@ import { useTheme } from 'styled-components';
 
 import { BackButton } from '../../components/BackButton';
 import { Button } from '../../components/Button';
+import { Calendar } from '../../components/Calendar';
 
 import ArrowSvg from '../../assets/arrow.svg';
 
@@ -49,7 +50,9 @@ export function Scheduling() {
         </RentalPeriod>
       </Header>
 
-      <Content></Content>
+      <Content>
+        <Calendar />
+      </Content>
 
       <Footer>
         <Button title="Confirmar" />
