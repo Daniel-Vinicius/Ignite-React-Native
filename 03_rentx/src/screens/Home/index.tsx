4f7b2cd97be38b2552a70595e9a5ae16b57ce685
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-09-09 21:18:27 -0300

    Memory leak

--- 03_rentx/src/screens/Home/index.tsx
+++ 03_rentx/src/screens/Home/index.tsx
@@ -31,19 +31,31 @@ export function Home() {
     navigation.navigate('CarDetails', { car });
   }
 
-  async function fetchCars() {
-    try {
-      const response = await api.get<CarDTO[]>('/cars');
-      setCars(response.data);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
   useEffect(() => {
+    let isMounted = true;
+
+    async function fetchCars() {
+      try {
+        const response = await api.get<CarDTO[]>('/cars');
+
+        if (isMounted) {
+          setCars(response.data);
+        }
+
+      } catch (error) {
+        console.log(error);
+      } finally {
+        if (isMounted) {
+          setLoading(false);
+        }
+      }
+    }
+
     fetchCars();
+
+    return () => {
+      isMounted = false;
+    };
   }, []);
 
   return (
