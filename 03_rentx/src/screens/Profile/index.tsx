5e2870665f2c7a43288213bcf7ca2b8dd47eab13
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-09-12 21:46:16 -0300

    Confirmação de Sign out

--- 03_rentx/src/screens/Profile/index.tsx
+++ 03_rentx/src/screens/Profile/index.tsx
@@ -94,6 +94,23 @@ export function Profile() {
     }
   }
 
+  async function handleSignOut() {
+    Alert.alert(
+      'Tem certeza?',
+      'Se você sair, irá precisar de internet para conectar-se novamente.',
+      [
+        {
+          text: 'Cancelar',
+          onPress: () => {}
+        },
+        {
+          text: 'Sair',
+          onPress: signOut
+        }
+      ]
+    );
+  }
+
   return (
     <KeyboardAvoidingView behavior="position" enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -102,7 +119,7 @@ export function Profile() {
             <HeaderTop>
               <BackButton color={theme.colors.shape} />
               <HeaderTitle>Editar Perfil</HeaderTitle>
-              <LogoutButton onPress={signOut}>
+              <LogoutButton onPress={handleSignOut}>
                 <Feather
                   name="power"
                   size={24}
