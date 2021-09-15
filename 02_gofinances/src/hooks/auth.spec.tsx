d42990675bd063927c8a96fcc67ecd2e9793e353
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-09-15 18:28:08 -0300

    Testes Unitários e de Integração

--- 02_gofinances/src/hooks/auth.spec.tsx
+++ 02_gofinances/src/hooks/auth.spec.tsx
@@ -52,4 +52,16 @@ describe('Auth Hook', () => {
 
     expect(result.current.user).not.toHaveProperty('id');
   });
+
+  it('should be error with incorrectly Google parameters', async () => {
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider
+    });
+
+    try {
+      await act(() => result.current.signInWithGoogle());
+    } catch (error) {
+      expect(result.current.user).not.toHaveProperty('id');
+    }
+  });
 });
