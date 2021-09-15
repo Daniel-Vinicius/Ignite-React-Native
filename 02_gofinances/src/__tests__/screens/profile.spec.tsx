a60b3ae685734d71d65b1bbf3124df638986dbeb
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-09-15 10:38:38 -0300

    Primeiro Teste

--- 02_gofinances/src/__tests__/screens/profile.spec.tsx
+++ 02_gofinances/src/__tests__/screens/profile.spec.tsx
@@ -4,12 +4,9 @@ import { render } from '@testing-library/react-native';
 import { Profile } from '../../screens/Profile';
 
 test('Check if show correctly user input name placeholder', () => {
-  const { getByPlaceholderText, debug } = render(<Profile />);
-
-  debug();
+  const { getByPlaceholderText } = render(<Profile />);
 
   const inputName = getByPlaceholderText('Nome');
-  console.log("### INPUT ###", inputName);
   
-  expect(inputName).toBeDefined();
+  expect(inputName.props.placeholder).toBeTruthy();
 });
