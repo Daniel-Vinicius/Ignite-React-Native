9f5da1e982e66ccfb6886ea384c35e20925e353f
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-09-17 19:39:35 -0300

    Memo

--- 04_performance/src/components/Friend.tsx
+++ 04_performance/src/components/Friend.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 import { Text } from 'react-native';
 
@@ -9,10 +9,27 @@ import { Text } from 'react-native';
    }
  }
 
-export function Friend({ data }: Props) {
+function FriendComponent({ data }: Props) {
   return (
     <Text>
       { data.name } - Likes: { data.likes }
     </Text>
   );
 };
+
+// export const Friend = memo(FriendComponent, ((prevProps, nextProps) => {
+//   const namePropNotChanged = prevProps.data.name === nextProps.data.name;
+//   const likesPropNotChanged = prevProps.data.likes === nextProps.data.likes;
+
+//   if (namePropNotChanged && likesPropNotChanged) {
+//     return true
+//   }
+
+//   return false;
+// }));
+
+export const Friend = memo(FriendComponent, ((prevProps, nextProps) => {
+  // if propsAreEqual dont'n recalcule this component
+  const propsAreEqual = Object.is(prevProps.data, nextProps.data);
+  return propsAreEqual;
+}));
