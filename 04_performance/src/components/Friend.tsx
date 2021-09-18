3a9662cb00eb925e40feeade0dba30db2b8aa4dc
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-09-18 18:04:42 -0300

    Bundle Visualizer

--- 04_performance/src/components/Friend.tsx
+++ 04_performance/src/components/Friend.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react';
 
 import { Text, View, TouchableOpacity } from 'react-native';
+import lodash from 'lodash';
 
 interface Props {
   data: {
@@ -26,6 +27,6 @@ function FriendComponent({ data, follow }: Props) {
 
 export const Friend = memo(FriendComponent, ((prevProps, nextProps) => {
   // if propsAreEqual dont'n recalcule this component
-  const propsAreEqual = Object.is(prevProps.data, nextProps.data);
+  const propsAreEqual = lodash.isEqual(prevProps.data, nextProps.data);
   return propsAreEqual;
 }));
