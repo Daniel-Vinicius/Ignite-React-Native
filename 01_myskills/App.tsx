a4784445541336b1c2ee7771d858f5b768b34535
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-09-30 17:28:40 -0300

    Carregamento da Splash

--- 01_myskills/App.tsx
+++ 01_myskills/App.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "react-native";
+
+import SplashScreen from 'react-native-splash-screen'
 
 import { Home } from "./src/pages/Home";
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#121015" />
